97b320ee53ac8bf8c27122f058e38934a6ce8541
Queniex <Queniesalbiyah@gmail.com>
2023-08-15 18:00:11 +0700

    Learning two-way data binding

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <h2>Welcome {{ first_name + " " + last_name }}!</h2>
   <button v-on:click="changeName">Change Name</button>
+    <!-- v-on could be written as @click  -->
+  <hr>
   <h3>My Github : <a v-bind:href="url"> Go to Link</a></h3>
-  <!-- v-on could be written as @click  -->
+  <input type="text" v-model="name">
+  <h1>{{ name }}</h1>
 </template>
 
 <script>
@@ -13,12 +16,13 @@ export default {
     return{
       first_name: "Quenie",
       last_name: "Salbiyah",
-      url: "https://github.com/Queniex"
+      url: "https://github.com/Queniex",
+      name: ""
     };
   },
   methods:{
     changeName(){
-      this.first_name = "Lailany";
+      this.first_name = "Back";
     }
   }
 }
